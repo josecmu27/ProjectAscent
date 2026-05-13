bd327b3819eef90d64defbbed906c7956577da14
josecmu27 <joselima@andrew.cmu.edu>
2026-05-13 16:20:13 -0400

    Initial Commit

---
+++ Source/ProjectAscent/ProjectAscent.cpp
@@ -0,0 +1,7 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#include "ProjectAscent.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, ProjectAscent, "ProjectAscent" );
+ 
