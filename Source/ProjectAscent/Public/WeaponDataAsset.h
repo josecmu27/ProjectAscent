48542584c3390c1232045d76a742edbfedd06bbe
josecmu27 <joselima@andrew.cmu.edu>
2026-05-19 19:18:52 -0400

    Feature: WeaponDataAsset with weapon stats

---
+++ Source/ProjectAscent/Public/WeaponDataAsset.h
@@ -0,0 +1,36 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Engine/DataAsset.h"
+#include "WeaponDataAsset.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class PROJECTASCENT_API UWeaponDataAsset : public UDataAsset
+{
+	GENERATED_BODY()
+	
+public:
+
+	UPROPERTY(EditDefaultsOnly, BlueprintReadOnly, Category = "Weapon Stats")
+	int32 Damage;
+
+	UPROPERTY(EditDefaultsOnly, BlueprintReadOnly, Category = "Weapon Stats")
+	int32 MagazineSize;
+
+	UPROPERTY(EditDefaultsOnly, BlueprintReadOnly, Category = "Weapon Stats")
+	float ReloadTime;
+
+	UPROPERTY(EditDefaultsOnly, BlueprintReadOnly, Category = "Weapon Stats")
+	float FireRate;
+
+	UPROPERTY(EditDefaultsOnly, BlueprintReadOnly, Category = "Weapon Stats")
+	float MaxRange;
+
+	UPROPERTY(EditDefaultsOnly, BlueprintReadOnly, Category = "Weapon Stats")
+	FText Name;
+};
